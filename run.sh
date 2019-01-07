a972eb950485c33873472c0d84f5cc331e751b99
root <root@node1.chmisa-qv45083.contools-pg0.utah.cloudlab.us>
2019-01-06 23:38:07 -0700

    added run script

---
+++ run.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+B="----------------"
+
+TARGET_IPV4="10.10.1.2"
+
+PING_ARGS="-D -i 1.0 -s 56"
+
+CONTAINER_COUNTS=( 1 2 )
+
+NATIVE_PING_CMD="${HOME}/contools-eval/iputils/ping"
+CONTAINER_PING_CMD="/iputils/ping"
+
+PING_CONTAINER_IMAGE="chrismisa/contools:ping-ubuntu"
+PING_CONTAINER_NAME="ping-container"
+
+PAUSE_CMD="sleep 5"
+PING_PAUSE_CMD="sleep 10"
+
+DATE_TAG=`date +%Y%m%d%H%M%S`
+META_DATA="Metadata"
+MANIFEST="manifest"
+
+mkdir $DATE_TAG
+cd $DATE_TAG
+
+# Get some basic meta-data
+echo "uname -a -> $(uname -a)" >> $META_DATA
+echo "docker -v -> $(docker -v)" >> $META_DATA
+echo "lsb_release -a -> $(lsb_release -a)" >> $META_DATA
+echo "sudo lshw -> $(sudo lshw)" >> $META_DATA
+
+#
+# Native run
+#
+echo $B Native control $B
+# Run ping in background
+echo "native_control_${TARGET_IPV4}.ping" >> $MANIFEST
+$NATIVE_PING_CMD $PING_ARGS $TARGET_IPV4 \
+  > native_control_${TARGET_IPV4}.ping &
+echo "  pinging. . ."
+
+$PAUSE_CMD
+
+PING_PID=`ps -e | grep ping | sed -E 's/ *([0-9]+) .*/\1/'`
+echo "  got ping pid: $PING_PID"
+
+$PING_PAUSE_CMD
+
+kill -INT $PING_PID
+echo "  killed ping"
+
+$PAUSE_CMD
+
+#
+# Container pings
+#
+
+for n_containers in ${CONTAINER_COUNTS[@]}; do
+
+	# Start ping container as service
+	i=1
+	while [ "$i" -le "$n_containers" ]; do
+		docker run -itd \
+		  --name=${PING_CONTAINER_NAME}${i} \
+		  --entrypoint=/bin/bash \
+		  $PING_CONTAINER_IMAGE > /dev/null
+		i=$(($i + 1))
+	done
+	echo $B Started $n_containers containers $B
+
+	$PAUSE_CMD
+
+	echo $B Container control $B
+
+	echo "${n_containers}containers_${TARGET_IPV4}.ping" >> $MANIFEST
+	docker exec ${PING_CONTAINER_NAME}1 \
+	  $CONTAINER_PING_CMD $PING_ARGS $TARGET_IPV4 \
+	  > ${n_containers}containers_${TARGET_IPV4}.ping &
+	echo "  pinging. . . "
+
+	$PAUSE_CMD
+
+	PING_PID=`ps -e | grep ping | sed -E 's/ *([0-9]+) .*/\1/'`
+	echo "  got ping pid: $PING_PID"
+
+	$PING_PAUSE_CMD
+
+	kill -INT $PING_PID
+	echo "  killed ping"
+
+	$PAUSE_CMD
+
+	# Stop the containers
+	i=1
+	while [ "$i" -le "$n_containers" ]; do
+		docker stop ${PING_CONTAINER_NAME}${i} > /dev/null
+		docker rm ${PING_CONTAINER_NAME}${i} > /dev/null
+		i=$(($i + 1))
+	done
+	echo $B Stopped $n_containers containers $B
+done
+
+echo Done.
